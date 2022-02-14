c79a0e87f0a8724e40346b0a6f1b752dcb1d1ffc
USER <USER@DESKTOP-0N68O5J>
2022-02-14 13:10:29 +0100

    Add project files.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            int table = 2;
+
+            for(int i = 0; length <= 100; length++)
+            {
+                i = length * 2;
+                Console.WriteLine("{0} x {1} = {2}", length, table, i);
+            }
+        }
+    }
+}
